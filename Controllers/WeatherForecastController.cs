e814bad2c99be871266eabf417d5ed86d98effa8
harborpirate (Brian Swift) <brian.swift.webdev@gmail.com>
2020-05-02 17:48:46 -0700

    Moved data model item.

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using bballsim.simulation.Model;
 
 namespace bballsim.simulation.Controllers
 {
--- Model/WeatherForecast.cs
+++ Model/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace bballsim.simulation
+namespace bballsim.simulation.Model
 {
     public class WeatherForecast
     {
